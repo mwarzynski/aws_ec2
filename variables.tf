d3b4f361147043ac3acdc025c2b8516cd3e75c70
Mateusz Warzyński <mathew.warzynski@gmail.com>
2020-02-09 14:35:03 +0100

    fix: Add descriptions at variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,16 @@
 variable "name" {
   default = "worker-instance"
-  type    = string
+  type = string
+  description = "Prefix for managed resources names."
 }
 
 variable "instance_type" {
   default = "t2.nano"
-  type    = string
+  type = string
+  description = "Type of the EC2 instance to create. (https://aws.amazon.com/ec2/instance-types/)"
 }
 
 variable "myip" {
-  type    = string
+  type = string
+  description = "IPv4 address of the host which should be allowed to SSH into the EC2 instance."
 }
